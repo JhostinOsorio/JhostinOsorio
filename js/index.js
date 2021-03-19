fb2f2d11de1b74937cf7caedf8d44a5ec46859de
Jhostin Osorio <66564971+JhostinOsorio@users.noreply.github.com>
2021-03-19 10:21:33 -0500

    Correcion de Banner y Header

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 const buttonToggle = document.getElementById("button-toggle");
 const mainNav = document.getElementById('main-nav');
 const menuListLinks = document.querySelectorAll('.menulist__link');
+const backgroundBanner = Array.from(document.querySelectorAll('.background-banner'));
 const main = document.querySelector('.main');
 const redes = document.getElementById('redes');
 const redesContainer = document.getElementById('redes--container');
 const year = document.getElementById('year');
+let indexBanner = 3;
 
 buttonToggle.addEventListener("click", (e) => {
   buttonToggle.classList.toggle("active");
@@ -32,3 +34,10 @@ redes.addEventListener('click', e => {
 })
 
 year.textContent = (new Date()).getFullYear();
+
+setInterval(() => {
+  backgroundBanner[indexBanner].classList.add('background-banner--opacity')
+  indexBanner--
+  if(indexBanner<0) indexBanner=3
+  backgroundBanner[indexBanner].classList.remove('background-banner--opacity')
+}, 4000);
